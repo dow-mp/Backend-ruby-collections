b0add4f3d6396adc979835706e1afd7e0ee0cbc4
dow-mp <derin.mitchell@gmail.com>
2023-04-02 13:06:46 -0400

    write add_up function to sum all integers in range from 1 to i using loop

--- add_up.rb
+++ add_up.rb
@@ -1,1 +1,16 @@
 # Write a program with a function add_up(i) .  It is to be passed a positive integer, and it will add all the numbers from 1 to that integer and return the sum.  Call the function three times within the program, and each time print out the return value.  Call the program add_up.rb.
+
+def add_up(i)
+    num = 0
+    count = 0
+    while count <= i
+        num += count
+        count += 1
+        # print [num, count]
+    end
+    return num
+end
+
+puts add_up(15)
+puts add_up(7)
+puts add_up(43)
